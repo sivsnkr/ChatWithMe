f26ae6adc90e58157b784f0b55fcee0ebb915662
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-09 11:26:02 +0530

    include socket.io and render the main html file

--- server/app.js
+++ server/app.js
@@ -1,2 +1,18 @@
 const express = require("express");
+const http = require("http");
+const io = require("socket.io");
+//configuring
 const app = express();
+const httpServer = http.Server(app);
+const ioServer = io(httpServer);
+
+//routes
+app.get("/",function(req,res){
+    res.sendFile(__dirname+'/index.html');
+})
+
+
+//listining to server
+app.listen(3000,function(){
+    console.log("Server is running....");
+})
